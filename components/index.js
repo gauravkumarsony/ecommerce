ae9d65a7919e5d304e1a0609aebdeae77e6d0bcc
Gaurav Kumar <gauravsony67@gmail.com>
2025-01-20 20:40:53 +0530

    created components and imported on index.js in pages

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { deffault as Footer } from './Footer';
+export { deffault as Layout } from './Layout';
+export { deffault as Navbar } from './Navbar';
+export { deffault as Product } from './Product';
+export { deffault as HeroBanner } from './HeroBanner';
+export { deffault as FooterBanner } from "./FooterBanner";
+export { deffault as Cart } from './Cart';
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner /> {/* called HeroBanner from components  */}
       <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div className='products-container'>{["Product 1", "Product 2"].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      <FooterBanner /> {/* called FooterBanner from components  */}
     </>
-  )
+  );
 }
 
 export default Home;
