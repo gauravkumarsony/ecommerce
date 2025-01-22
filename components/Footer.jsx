47eea77ae120b2ec31f115b28e294fa01efbc486
Gaurav Kumar <gauravsony67@gmail.com>
2025-01-22 12:35:26 +0530

    added footer icons with links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import { FaGlobe } from "react-icons/fa6";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2025 GK Headphones All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://gaurav-gk.netlify.app/">
+          <FaGlobe />
+        </a>
+        <a href="https://www.linkedin.com/in/gauravkumarsony/">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/gauravkumarsony">
+          <AiFillGithub />
+        </a>
       </p>
     </div>
-  )
+  );
 }
 
 export default Footer
