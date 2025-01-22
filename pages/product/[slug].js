6a2f59b344f9eee0bbd68c899698aef2557dd020
Gaurav Kumar <gauravsony67@gmail.com>
2025-01-22 13:03:05 +0530

    created file based routing as new [slug].js file, and created productdetails

---
+++ pages/product/[slug].js
@@ -0,0 +1,11 @@
+//this is file based routing in next.js
+// we dont have implement any kind of library, like ract router 
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default ProductDetails;
